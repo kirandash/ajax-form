4d821b91eb9114e95e40a11fb56a0d0172af1b70
kirandash <thekirankumardash@gmail.com>
2016-07-30 07:39:01 +0530

    Retrieving multiple data.

--- basic/ajax_target.php
+++ basic/ajax_target.php
@@ -1,3 +1,19 @@
 <?php
 	echo 'This is the data to be retrieved.';
+	echo '<br>';
+	echo '<br>';
+	print 'this is dummy<br>';
+	
+	$array[] ="kiran";
+	$array[] ="kiran1";	
+	$array[] ="kiran2";
+	$array[] ="kiran3";
+	$array[] ="kiran4";
+	$array[] ="kiran5";
+	$array[] ="kiran6";
+	$array[] ="kiran7";
+	
+	foreach ($array as $arr){
+		echo $arr.'<br><br>';
+	}
 ?>
